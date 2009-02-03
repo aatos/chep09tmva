32cf9d6104ae3f93c728e127e68b0b24f392a0d8
aatos heikkinen <aatos.heikkinen@cern.ch>
2009-02-03 11:49:13 +0200

    Test target added. ROOT login and logoff scripts added.

---
+++ code/rootlogon.C
@@ -0,0 +1,6 @@
+{
+   printf("\nWelcome to Ideal tau-tagging with TMVA \n\n");
+   printf("\nType \".x TMVAGui.C\" for data analysis menu\n");
+   printf("\n(Analysis must be run before. e.g. 'make test')\n");
+}
+
