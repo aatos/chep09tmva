4baa24d681bb80215cbd51d54b780819091dff3a
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2009-03-09 11:27:44 +0200

    Improved csv.C script
    Added support for 3D-visualization.

--- code/scripts/csv.C
+++ code/scripts/csv.C
@@ -6,13 +6,18 @@
 #include <TCanvas.h>
 #include <TLatex.h>
 
-int textColor(double value, double max) {
-  double f = value/max;
-  int ncolors = gStyle->GetNumberOfColors();
-  return gStyle->GetColorPalette(int(f*(ncolors-1)));
+int textColor(double value, double max, double min) {
+  if(max > min) {
+    double f = (value-min)/(max-min);
+    int ncolors = gStyle->GetNumberOfColors();
+    return gStyle->GetColorPalette(int(f*(ncolors-1)));
+  }
+  else {
+    return 1;
+  }
 }
 
-void csv(TString input="tmva.csvoutput.txt", TString par1="par2", TString par2="par3", TString value="eventEffScaled") {
+void csv(TString input="tmva.csvoutput.txt", TString par1="par2", TString par2="par3", TString par3="", TString value="eventEffScaled_5") {
   std::cout << "Usage:" << std::endl
             << ".x scripts/csv.C    with default arguments" << std::endl
             << ".x scripts/csv.C(filename, par1, par2, value)" << std::endl
@@ -36,7 +41,11 @@ void csv(TString input="tmva.csvoutput.txt", TString par1="par2", TString par2="
   tree->SetMarkerColor(kRed);
   if(par2.Length() > 0) {
     //tree->Draw(Form("%s:%s", par2.Data(), par1.Data()));
-    tree->Draw(Form("%s:%s:%s", par2.Data(), par1.Data(), value.Data()), "", "COLZ"); //, "", "Z");
+    if(par3.Length() > 0)
+      tree->Draw(Form("%s:%s:%s:%s", par1.Data(), par2.Data(), par3.Data(), value.Data()), "", "COLZ"); //, "", "Z");
+    else
+      tree->Draw(Form("%s:%s:%s", par2.Data(), par1.Data(), value.Data()), "", "COLZ"); //, "", "Z");
+
     TH1 *histo = tree->GetHistogram();
     if(!histo)
       return;
@@ -44,26 +53,31 @@ void csv(TString input="tmva.csvoutput.txt", TString par1="par2", TString par2="
     histo->SetTitle(Form("%s with different classifier parameters", value.Data()));
     histo->GetXaxis()->SetTitle(Form("Classifier parameter %s", par1.Data()));
     histo->GetYaxis()->SetTitle(Form("Classifier parameter %s", par2.Data()));
-    histo->GetZaxis()->SetTitle("");
+    if(par3.Length() > 0)
+      histo->GetZaxis()->SetTitle(Form("Classifier parameter %s", par3.Data()));
+    else
+      histo->GetZaxis()->SetTitle("");
 
-    float x = 0;
-    float y = 0;
-    float val = 0;
-    double maxVal = tree->GetMaximum(value);
+    if(par3.Length() == 0) {
+      float x = 0;
+      float y = 0;
+      float val = 0;
+      double maxVal = tree->GetMaximum(value);
+      double minVal = tree->GetMinimum(value);
 
-    tree->SetBranchAddress(par1, &x);
-    tree->SetBranchAddress(par2, &y);
-    tree->SetBranchAddress(value, &val);
-
-    TLatex l;
-    l.SetTextSize(0.03);
+      tree->SetBranchAddress(par1, &x);
+      tree->SetBranchAddress(par2, &y);
+      tree->SetBranchAddress(value, &val);
+      TLatex l;
+      l.SetTextSize(0.03);
     
-    Long64_t nentries = tree->GetEntries();
-    for(Long64_t entry=0; entry < nentries; ++entry) {
-      tree->GetEntry(entry);
+      Long64_t nentries = tree->GetEntries();
+      for(Long64_t entry=0; entry < nentries; ++entry) {
+        tree->GetEntry(entry);
     
-      l.SetTextColor(textColor(val, maxVal));
-      l.DrawLatex(x, y, Form("%.3f", val*100));
+        l.SetTextColor(textColor(val, maxVal, minVal));
+        l.DrawLatex(x, y, Form("%.3f", val*100));
+      }
     }
   }
   else {
